acb6af264ef54be47e31bf6c3e3d16d2192ebbe8
Fabian Angst <kontakt@angst-itc.de>
2021-12-18 11:29:44 +0100

    * ESLint hinzugefügt und konfiguriert * ESLint fixes ausgeführt

--- src/bo4e-lib.spec.ts
+++ src/bo4e-lib.spec.ts
@@ -1,18 +1,18 @@
-import { Bo4eLib } from './bo4e-lib';
+import {Bo4eLib} from './bo4e-lib';
 
 describe('Bo4eLib', () => {
-    let lib: Bo4eLib;
+  let lib: Bo4eLib;
 
-    beforeEach(() => {
-        lib = new Bo4eLib();
-    });
+  beforeEach(() => {
+    lib = new Bo4eLib();
+  });
 
-    it('should create', () => {
-        expect(lib).toBeTruthy();
-    });
+  it('should create', () => {
+    expect(lib).toBeTruthy();
+  });
 
-    it('should be version 11', () => {
-        expect(lib.getVersion()).toEqual(11);
-        expect(Bo4eLib.VERSION).toEqual(11);
-    });
-})
+  it('should be version 11', () => {
+    expect(lib.getVersion()).toEqual(11);
+    expect(Bo4eLib.VERSION).toEqual(11);
+  });
+});
--- src/bo4e-lib.ts
+++ src/bo4e-lib.ts
@@ -2,13 +2,14 @@
  * Hauptklasse der BO4E-Typescript Library, welche Informationen zur BO4E-Version bereitstellt.
  */
 export class Bo4eLib {
-    /** Aktuelle Version des implementierten BO4E-Standards */
-    public static readonly VERSION = 11;
+  /** Aktuelle Version des implementierten BO4E-Standards */
+  public static readonly VERSION = 11;
 
-    /**
-     * Liefert die Version des aktuell implementierten BO4E Standards zurück.
-     */
-    public getVersion(): number {
-        return Bo4eLib.VERSION;
-    }
+  /**
+   * Liefert die Version des aktuell implementierten BO4E Standards zurück.
+   * @return {number} BO4ELib Version
+   */
+  public getVersion(): number {
+    return Bo4eLib.VERSION;
+  }
 }
